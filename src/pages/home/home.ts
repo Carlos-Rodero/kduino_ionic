855028fea7ea897febd9392b62fa4401c2d9d324
Carlos-Rodero <carlos.rodero.garcia@gmail.com>
2018-05-15 00:47:58 +0200

    primer GET

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  posts: any;
+
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.posts = null;
+ 
+    this.http.get('http://192.168.4.1/c').map(res => res.json()).subscribe(data => {
+        this.posts = data.station;
+        console.log(this.posts);
+  });
 
   }
 
