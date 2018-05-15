865dcb473682f74c11a5a142e194f60ab10672e6
Carlos-Rodero <carlos.rodero.garcia@gmail.com>
2018-05-15 18:09:56 +0200

    page Play

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { PlayPage } from '../pages/play/play';
+import { AboutPage } from '../pages/about/about';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -15,9 +16,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    PlayPage,
+    AboutPage,
     TabsPage
   ],
   imports: [
@@ -28,9 +29,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    PlayPage,
+    AboutPage,
     TabsPage
   ],
   providers: [
---
+++ src/pages/play/play.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/**
+ * Generated class for the PlayPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-play',
+  templateUrl: 'play.html',
+})
+export class PlayPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+
+}
